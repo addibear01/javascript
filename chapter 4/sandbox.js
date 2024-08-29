c856e96590552220786b133659cc0f9be4ecdb49
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 13:22:22 -0500

    starting chapter 4 with functions

---
+++ chapter 4/sandbox.js
@@ -0,0 +1,25 @@
+// function desclaration
+// function greet(){
+//     console.log('hello there');
+// }
+
+//function expression
+// const speak = function(){
+//     console.log('good day!');
+// };
+
+// greet();
+// greet();
+// greet();
+
+speak();
+speak();
+speak();
+
+function greet(){
+    console.log('hello there');
+}
+
+const speak = function(){
+    console.log('good day!');
+};
