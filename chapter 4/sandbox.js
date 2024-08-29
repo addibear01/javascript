cf234a94ab3329f784e5da1459bbec0bb20cf39a
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 13:42:33 -0500

    did some conversion from regular to arrow functions

--- chapter 4/sandbox.js
+++ chapter 4/sandbox.js
@@ -33,15 +33,53 @@
 // speak('shaun', 'day');
 
 //returning values
-const calcArea = function(radius){
-    return 3.14 * radius**2;
-}
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+// const area = calcArea(5);
+// console.log(area);
+
+// const calcVol = function(area){
+
+// };
+
+// calcVol(area);
+
+// regular function
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+//arrow function
+const calcArea = radius => 3.14 * radius**2;
 
 const area = calcArea(5);
-console.log(area);
+console.log('area is:', area);
 
-const calcVol = function(area){
+//regular function
+// const greet = function(){
+//     return 'hello, world';
+// };
+//arrow function
+// const greet = () => 'hello, world';
+// const result = greet();
+// console.log(result);
 
-};
-
-calcVol(area);
+//regular function
+// const bill = function(proudcts, tax){
+//     let total = 0;
+//     for(let i =0; i < proudcts.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+//arrow function
+// const bill = (products,tax) => {
+//     let total = 0;
+//     for(let i =0; i < proudcts.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+// console.log(bill([10,15,30], 0.2));
