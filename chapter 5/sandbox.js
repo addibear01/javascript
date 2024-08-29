668ae922f3c9a2d47b5dd3102ea340785063342c
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 14:24:47 -0500

    Finished math built in function

--- chapter 5/sandbox.js
+++ chapter 5/sandbox.js
@@ -1,40 +1,63 @@
 // object literals
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'crystal@thenetninja.co.uk',
-    location: 'berlin',
-    blogs: ['why mac and cheese rules', '10 things to make with marmite'],
-    login(){
-        console.log('the user logged in');
-    },
-    logout(){
-        console.log('the user logged out');
-    },
-    logBlogs(){
-        console.log(this.blogs);
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => {
-            console.log(blog);
-        })
-    }
-};
+// const blogs = [
+//     {title: 'why mac and cheese rules', likes: 30},
+//     {title: '10 things to make with marmite', likes: 50}
+// ];
 
-user.logBlogs();
-console.log(this);
-user.login();
-user.logout();
-const name = 'mario';
-name.toUpperCase();
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'crystal@thenetninja.co.uk',
+//     location: 'berlin',
+//     blogs: [{title: 'why mac and cheese rules', likes: 30},
+//             {title: '10 things to make with marmite', likes: 50}],
+//     login(){
+//         console.log('the user logged in');
+//     },
+//     logout(){
+//         console.log('the user logged out');
+//     },
+//     logBlogs(){
+//         console.log(this.blogs);
+//         console.log('this user has written the following blogs:');
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes);
+//         })
+//     }
+// };
 
-console.log(user);
-console.log(user.name);
-user.age = 35;
-console.log(user.age);
-const key = 'location';
-console.log(user['location']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+// user.logBlogs();
+// console.log(this);
+// user.login();
+// user.logout();
+// const name = 'mario';
+// name.toUpperCase();
 
-console.log(typeof user);
+// console.log(user);
+// console.log(user.name);
+// user.age = 35;
+// console.log(user.age);
+// const key = 'location';
+// console.log(user['location']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
+//Math object
+console.log(Math);
+console.assertlog(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+//random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
