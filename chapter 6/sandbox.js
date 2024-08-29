bd08450f53d5c54301149966ddd169a8f956e839
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 15:08:25 -0500

    Added styles to attributes

--- chapter 6/sandbox.js
+++ chapter 6/sandbox.js
@@ -2,9 +2,55 @@
 
 // console.log(para);
 
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
 // paras.forEach(para => {
 //     console.log(para);
 // })
-console.log(paras[2]);
+// console.log(paras[2]);
+
+//get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+//get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+//get elements by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+// const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome!';
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
+
+// const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>';
+
+// const people = ['mario', 'luigi', 'yoshi'];
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`;
+// });
+
+// p2 attributes
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+// link.innerText = 'The Net Ninja Website';
+// const mssg = document.querySelector('p');
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'success');
+// mssg.setAttribute('style', 'color: green;');
+
