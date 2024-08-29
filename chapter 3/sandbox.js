b7b2f02621d98e3c65361f137b8072418ec93553
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 11:53:23 -0500

    Tried block scope and now finished chapter 3

--- chapter 3/sandbox.js
+++ chapter 3/sandbox.js
@@ -100,25 +100,41 @@
 // }
 
 // switch statements
-const grade = 'D';
+// const grade = 'D';
 
-switch(grade){
-    case 'A':
-        console.log('you got an A!');
-        break;
-    case 'B':
-        console.log('you got an B!');
-        break;
-    case 'C':
-        console.log('you got an C!');
-        break;
-    case 'D':
-        console.log('you got an D!');
-        break;
-    case 'E':
-        console.log('you got an E!');
-        break;
-    default: console.log('not a valid grade');
+// switch(grade){
+//     case 'A':
+//         console.log('you got an A!');
+//         break;
+//     case 'B':
+//         console.log('you got an B!');
+//         break;
+//     case 'C':
+//         console.log('you got an C!');
+//         break;
+//     case 'D':
+//         console.log('you got an D!');
+//         break;
+//     case 'E':
+//         console.log('you got an E!');
+//         break;
+//     default: console.log('not a valid grade');
+// }
+
+// variables and block scope
+const age = 30;
+
+if(true){
+    const age = 40;
+    const name = 'shaun';
+    console.log('inside 1st code block: ', age, name);
+
+    if(true){
+        const age = 50;
+        console.log('inside second code block: ', age)
+        var test = 'hello';
+    }
 }
+console.log('outside code block: ', age, name, test);
 
 
