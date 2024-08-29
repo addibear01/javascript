58268f9867b00c277dd8280bfec1279fbe8376c9
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 11:44:29 -0500

    Practised switch statements

--- chapter 3/sandbox.js
+++ chapter 3/sandbox.js
@@ -87,16 +87,38 @@
 // console.log(!false);
 
 // break and continue
-const scores = [50, 25, 0, 30, 100, 20, 10];
-for(let i = 0; i < scores.length; i++){
-    if(scores[i] === 0){
-        continue;
-    }
-    console.log('your score: ', scores[i]);
-    if(scores[i] === 100){
-        console.log('congrats, you got the top score!');
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+// for(let i = 0; i < scores.length; i++){
+//     if(scores[i] === 0){
+//         continue;
+//     }
+//     console.log('your score: ', scores[i]);
+//     if(scores[i] === 100){
+//         console.log('congrats, you got the top score!');
+//         break;
+//     }
+// }
+
+// switch statements
+const grade = 'D';
+
+switch(grade){
+    case 'A':
+        console.log('you got an A!');
         break;
-    }
+    case 'B':
+        console.log('you got an B!');
+        break;
+    case 'C':
+        console.log('you got an C!');
+        break;
+    case 'D':
+        console.log('you got an D!');
+        break;
+    case 'E':
+        console.log('you got an E!');
+        break;
+    default: console.log('not a valid grade');
 }
 
 
