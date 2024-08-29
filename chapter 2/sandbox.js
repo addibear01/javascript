7c7487d987114e77b50ae72bf1f29e53f7500512
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 10:34:23 -0500

    Practised comparison

--- chapter 2/sandbox.js
+++ chapter 2/sandbox.js
@@ -151,12 +151,26 @@
 // console.log(age <= 25);
 // console.log(age >= 25);
 
-let name = 'shaun';
-console.log(name == 'shaun');
-console.log(name == 'Shaun');
-console.log(name > 'crystal');
-console.log(name > 'Shaun');
-console.log(name > 'Cystal');
+// let name = 'shaun';
+// console.log(name == 'shaun');
+// console.log(name == 'Shaun');
+// console.log(name > 'crystal');
+// console.log(name > 'Shaun');
+// console.log(name > 'Cystal');
+
+// let age = 25;
+
+//loose comparison (different types can still be equal)
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+//strict comparison (different types cannot be equal)
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
 
 
 
