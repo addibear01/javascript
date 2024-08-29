23f5b43c4678f27eb6a05a8dd1b3fe457b5df6ee
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 11:31:36 -0500

    Added logical operators

--- chapter 3/sandbox.js
+++ chapter 3/sandbox.js
@@ -56,13 +56,25 @@
 // }
 
 // else if statements
-const password = 'p@ss';
+// const password = 'p@ss';
 
-if(password.length >= 12){
+// if(password.length >= 12){
+//     console.log('that password is mighty strong');
+// } else if(password.length >= 8){
+//     console.log('that password is long enough!');
+// } else {
+//     console.log('password is not long enough');
+// }
+
+//logical operators
+const password = 'p@ss12';
+
+if(password.length >= 12 && password.includes('@')){
     console.log('that password is mighty strong');
-} else if(password.length >= 8){
-    console.log('that password is long enough!');
+} else if(password.length >= 8 || password.includes('@') && password.length >= 5){
+    console.log('that password is strong enough!');
 } else {
     console.log('password is not long enough');
 }
+
 
