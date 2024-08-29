14071957ed797dc4327026ac18adc251665123b3
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 14:01:40 -0500

    quickly made the regular function into an arrow function

--- chapter 4/sandbox.js
+++ chapter 4/sandbox.js
@@ -122,7 +122,7 @@ const ul = document.querySelector('.people');
 const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 let html = ``;
 
-people.forEach(function(person){
+people.forEach(person => {
     //create html template
     html += `<li style="color: purple">${person}</li>`;
 });
