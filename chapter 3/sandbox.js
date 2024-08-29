a6a8162a330ebffb18497794b664d46af3d0778c
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 11:09:40 -0500

    did while, do while and if statements

--- chapter 3/sandbox.js
+++ chapter 3/sandbox.js
@@ -5,10 +5,51 @@
 
 // console.log('loop finished');
 
-const names = ['shaun', 'mario', 'luigi'];
+//for loop
+// const names = ['shaun', 'mario', 'luigi'];
 
-for(let i = 0; i < names.length; i++){
-    //console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
+// for(let i = 0; i < names.length; i++){
+//     //console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`;
+//     console.log(html);
+// }
+
+//while loop
+// let i = 0;
+// while(i < 5){
+//     console.log('in loop: ', i);
+//     i++;
+// }
+
+// const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+// do while loops
+// let i = 5;
+// do{
+//     console.log('val of i is: ', i);
+//     i++;
+// } while(i < 5);
+
+// if statements
+// const age = 25;
+
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//     console.log('thats a lot of ninjas');
+// }
+
+const password = 'pass';
+
+if(password.length >= 8){
+    console.log('that password is long enough!');
 }
