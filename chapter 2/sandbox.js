a99f2331b3152e3369bd127aa03ba17dd7dab3cc
addibear01 <149608305+addibear01@users.noreply.github.com>
2024-08-29 09:14:38 -0500

    Added more javascript using cool javascript methods

--- chapter 2/sandbox.js
+++ chapter 2/sandbox.js
@@ -1,17 +1,45 @@
 // alert('hello, world');
 // console.log(1);
 // console.log(2);
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-// log things to console
-console.log(age, year);
+// // log things to console
+// console.log(age, year);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
 
-const points = 100;
-console.log(points);
+// const points = 100;
+// console.log(points);
 
-var score = 75;
-console.log(score);
+// var score = 75;
+// console.log(score);
+
+// strings
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+// string concatenation
+
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
